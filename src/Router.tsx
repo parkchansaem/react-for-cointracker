5edf39d60d7a99460bc38802d658003e70cef221
chansaem <ased1233@naver.com>
2022-01-26 11:32:39 +0900

    publish

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/:coinId/*" element={<Coin />}></Route>
         <Route path="/" element={<Coins />}></Route>
